7ebf88b551dfd6d4f7962213462a95b632b0ac8f
Alikhan <rainhardt_rauenaltez@bk.ru>
2025-03-08 03:56:14 +0500

    no such

--- src/components/guides/Guides.jsx
+++ src/components/guides/Guides.jsx
@@ -1,17 +1,26 @@
 import "./Guides.css";
 
+import {
+  IoBookOutline,
+  IoPeopleOutline,
+  IoMapOutline,
+  IoCartOutline,
+  IoChatbubbleEllipsesOutline,
+  IoReaderOutline,
+} from "react-icons/io5";
+
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import { NavLink } from "react-router";
 
 const Guides = ({ onClose }) => {
   const guides = [
-    { name: "Меню", path: "/menu" },
-    { name: "Вакансии", path: "/vacancies" },
-    { name: "Контакты", path: "/contacts" },
-    { name: "Корзина", path: "/cart" },
-    { name: "Отзывы", path: "/reviews" },
-    { name: "Бланк", path: "/receipt" },
+    { name: "Меню", icon: <IoBookOutline />, path: "/menu" },
+    { name: "Вакансии", icon: <IoPeopleOutline />, path: "/vacancies" },
+    { name: "Контакты", icon: <IoMapOutline />, path: "/contacts" },
+    { name: "Корзина", icon: <IoCartOutline />, path: "/cart" },
+    { name: "Отзывы", icon: <IoChatbubbleEllipsesOutline />, path: "/reviews" },
+    { name: "Бланк", icon: <IoReaderOutline />, path: "/receipt" },
   ];
 
   useEffect(() => {
@@ -28,9 +37,10 @@ const Guides = ({ onClose }) => {
         <div className="guides-content" onClick={(e) => e.stopPropagation()}>
           <div className="guides-label">Redux</div>
           <div className="guides-storage">
-            {guides.map(({ name, path }) => (
+            {guides.map(({ name, icon, path }) => (
               <NavLink key={path} className="guide" to={path} onClick={onClose}>
-                {name}
+                <div className="guide-icon">{icon}</div>
+                <div>{name}</div>
               </NavLink>
             ))}
           </div>
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -8,40 +8,46 @@ const Search = () => {
   const products = useProducts();
   const [query, setQuery] = useState("");
 
-  const filteredProducts = products.categories.flatMap(({ subcategories }) =>
-    subcategories
-      .map(({ name, items }) => ({
-        name,
-        items: items.filter(({ name }) =>
-          name.toLowerCase().includes(query.toLowerCase())
-        ),
-      }))
-      .filter(({ items }) => items.length)
+  const filteredProducts = products.categories.flatMap(
+    ({ name: categoryName, subcategories }) =>
+      subcategories.flatMap(({ name: subcategoryName, items }) =>
+        items
+          .filter(({ name }) =>
+            name.toLowerCase().includes(query.toLowerCase())
+          )
+          .map((item) => ({
+            categoryName,
+            subcategoryName,
+            item,
+          }))
+      )
   );
 
   return (
-    <>
-      <div className="search">
-        <div className="search-dashboard">
-          <input
-            className="search-field"
-            value={query}
-            placeholder="Название блюд..."
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <IoClose className="search-clear" onClick={() => setQuery("")} />
-        </div>
-
-        <div className="search-storage">
-          {filteredProducts.map(({ name, items }) => (
-            <div className="search-storage-section" key={name}>
-              <div>{name}</div>
-              <MC round={items} />
-            </div>
-          ))}
-        </div>
+    <div className="search">
+      <div className="search-dashboard">
+        <input
+          className="search-field"
+          value={query}
+          placeholder="Название блюда..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <IoClose className="search-clear" onClick={() => setQuery("")} />
       </div>
-    </>
+
+      <div className="search-storage">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map(({ categoryName, subcategoryName, item }) => (
+            <div className="search-storage-section" key={item.id}>
+              {categoryName} = {subcategoryName}
+              <MC round={[item]} />
+            </div>
+          ))
+        ) : (
+          <div className="search-empty">Ничего не найдено</div>
+        )}
+      </div>
+    </div>
   );
 };
 
