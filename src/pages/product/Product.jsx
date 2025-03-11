2a056803fafab3b17ab949a33e3f54d3a68c0539
Alikhan <rainhardt_rauenaltez@bk.ru>
2025-03-11 22:02:02 +0500

    such

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -5,7 +5,6 @@ import { useParams } from "react-router";
 import { useProducts } from "../../contexts/productsContext/productsContext";
 
 import Navigate from "../../components/navigate/Navigate";
-import SC from "../../components/cards/solo-card/Solo-Card";
 import Nomercy from "../../components/nomercy/Nomercy";
 
 const Product = () => {
@@ -13,12 +12,23 @@ const Product = () => {
   const line = useProducts()
     .categories.flatMap((c) => c.subcategories.flatMap((s) => s.items))
     .find((i) => i.id === product);
-    
+
   return (
     <>
       <div className="product">
         <Navigate />
-        <SC round={line} />
+        <div className="product-storage">
+          <div className="p-s-image"></div>
+          <div className="p-s-data">
+            <div className="p-s-named">{line.name}</div>
+            <div className="p-s-details">{line.details}</div>
+          </div>
+          <div className="p-s-counter">
+            <div>-</div>
+            <div>6</div>
+            <div>+</div>
+          </div>
+        </div>
         <Nomercy />
       </div>
     </>
